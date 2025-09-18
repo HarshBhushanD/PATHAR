5cef79cd9f50579ecd9017015a67b28c41c526fe
HarshBhushanD <154004374+HarshBhushanD@users.noreply.github.com>
2025-09-18 22:58:08 +0530

    up

--- firebase.js
+++ firebase.js
@@ -33,15 +33,26 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+//  Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLFO-hxE7wNzSKAeEfUIp5Pyrw3gChcIQ",
+//   authDomain: "pathar-d68ca.firebaseapp.com",
+//   projectId: "pathar-d68ca",
+//   storageBucket: "pathar-d68ca.appspot.com", 
+//   messagingSenderId: "534190904787",
+//   appId: "1:534190904787:web:6219b44edbc48d15730b26"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDLFO-hxE7wNzSKAeEfUIp5Pyrw3gChcIQ",
-  authDomain: "pathar-d68ca.firebaseapp.com",
-  projectId: "pathar-d68ca",
-  storageBucket: "pathar-d68ca.appspot.com", 
-  messagingSenderId: "534190904787",
-  appId: "1:534190904787:web:6219b44edbc48d15730b26"
+  apiKey: "AIzaSyDq0W1cUcgXppi0n-djWf5GtAhpXyz66eg",
+  authDomain: "pathar-c2e42.firebaseapp.com",
+  projectId: "pathar-c2e42",
+  storageBucket: "pathar-c2e42.firebasestorage.app",
+  messagingSenderId: "257970921464",
+  appId: "1:257970921464:web:d9333a09149191e106c00d",
+  measurementId: "G-ZFREPCCKCP"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
